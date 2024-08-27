5aaf992511c59529ef3acebbf5b8337a82dc0e57
Guilherme Silva <70703481+GSilva-N@users.noreply.github.com>
2024-08-26 21:00:24 -0300

    atividade

--- src/imc-calculator/imc-calculator.controller.ts
+++ src/imc-calculator/imc-calculator.controller.ts
@@ -26,4 +26,34 @@ export class ImcCalculatorController {
   calculate(@Body() request: ImcCalculatorRequest) {
     return this.imcCalcService.calculateAndTranslate(request);
   }
+
+  @Get()
+  @Render('imc-form')
+  showForm() {
+    return { fields: [
+      { label: 'Altura (em metros):', type: 'number', name: 'altura' },
+      { label: 'Peso (em kg):', type: 'number', name: 'peso' },
+    ]};
+  }
+
+  @Post()
+  @Render('imc-result')
+  calculateImc(@Body() body: { altura: string; peso: string }) {
+    const altura = parseFloat(body.altura);
+    const peso = parseFloat(body.peso);
+    const imc = peso / (altura * altura);
+    let categoria = '';
+
+    if (imc < 18.5) {
+      categoria = 'Abaixo do peso';
+    } else if (imc >= 18.5 && imc < 24.9) {
+      categoria = 'Peso normal';
+    } else if (imc >= 25 && imc < 29.9) {
+      categoria = 'Sobrepeso';
+    } else {
+      categoria = 'Obesidade';
+    }
+
+    return { imc: imc.toFixed(2), categoria };
+  }
 }
